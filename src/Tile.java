2579121540ec30bdbf593a6b54319114a91fa9ce
Gabi Tessier <mtessier246@gmail.com>
2019-11-23 12:51:17 -0500

    tile obj

---
+++ src/Tile.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+
+/**
+ * This class create a tile object
+ * @author Gabi
+ *
+ */
+
+public class Tile implements GameObject {
+	
+	/**
+	 * The width of the image
+	 */
+	public static final int IMG_WIDTH = 100;
+	/**
+	 * The height of the image.
+	 */
+	public static final int IMG_HEIGHT = 5;
+	/**
+	 * The position of center of the image.
+	 */
+	public static final double CENTER_Y = 0.8;
+	/**
+	 * The of center of the image X.
+	 */
+	public static final double CENTER_X = 0.7;
+	
+	
+	public void position () {
+		
+	}
+
+}
